de58e6bde50d338d0bb5d08cba619631e0742648
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-11 16:47:11 +0100

    completato codice javascript

--- script.js
+++ script.js
@@ -12,4 +12,17 @@ if (flag){
     console.log("La tua mail è valida");
 } else {
     console.log("La tua mail non è valida");
+    let permesso = prompt("Vuoi inserire la tua mail nella lista d'accesso? y (yes), n (no)");
+    switch (permesso){
+        case "y":
+            email.push(emailUtente);
+            console.log("Benvenuto");
+            console.log(email);
+            break
+        case "n":
+            console.log("Ci dispiace, arrivederci");
+            break
+        default:
+            console.log("Input non riconosciuto");
+    };
 };
